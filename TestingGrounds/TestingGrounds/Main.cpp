943ab2ea28e540fd3fa872d03d54d90332283634
Augusto Celis <31678229+AugustCell@users.noreply.github.com>
2022-03-10 12:58:46 -0700

    Adding text files explaining some more functinality. (#2)

--- TestingGrounds/TestingGrounds/Main.cpp
+++ TestingGrounds/TestingGrounds/Main.cpp
@@ -1,22 +1,62 @@
-#include "LinkedList.h"
+#define _CRTDBG_MAP_ALLOC
+#include "DoublyLinkedList.h"
+#include "SinglyLikedList.hpp"
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
 
 int main() {
 
 	cout << "Hello friends!" << endl;
 
-	LinkedList ll;
+	DoublyLinkedList dLl;  //This is our implemented doubly linked list
+    SinglyLinkedList sLl;  //This is our implemented singly linked list
+	
+    // Normal element insertion (S_LL)
+	sLl.insert(0);
+	sLl.insert(1);
+	sLl.insert(2);
+	sLl.insert(3);
+	dLl.insert(4);
 
-	// Normal element insertion
-	ll.insert(0);
-	ll.insert(1);
-	ll.insert(2);
-	ll.insert(3);
+	// Print normal insertion (S_LL)
+	sLl.printList();
 
-	// Print normal insertion
-	ll.printList();
+	// Testing reverse (S_LL)
+	sLl.reverse();
+	sLl.printList();
 
-	// Testing reverse
-	ll.reverse();
-	ll.printList();
+    // Testing middle element (S_LL)
+    cout << endl << "This is middle element: " << sLl.getMiddle() << endl;
+
+	// Normal element insertion (D_LL)
+	dLl.insert(0);
+	dLl.insert(1);
+	dLl.insert(2);
+	dLl.insert(3);
+	dLl.insert(4);
+
+	// Print normal insertion (D_LL)
+	dLl.printList();
+
+	// Testing reverse (D_LL)
+	dLl.reverse();
+	dLl.printList();
+
+    // Testing middle element (D_LL)
+    cout << endl << "This is middle element: " << sLl.getMiddle() << endl;
+
+    // Free whole list
+    dLl.destroyList();
+    sLl.destroyList();
+
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
---
+++ TestingGrounds/TestingGrounds/SinglyLikedList.hpp
@@ -0,0 +1,219 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <cstdint>
+#include "Node.hpp"
+
+using namespace std;
+
+class SinglyLinkedList
+{
+private:
+    SingleNode* head;
+    SingleNode* tail;
+    uint16_t size;
+
+public:
+    SinglyLinkedList() :
+        head(nullptr),
+        tail(nullptr),
+        size(0)
+    {
+    }
+
+    // O(1)
+    void addToFront(uint32_t value)
+    {
+        if (head == nullptr && tail == nullptr)
+        {
+            head = tail = new SingleNode(value);
+        }
+        else if (head == tail)
+        {
+            SingleNode* newHead = new SingleNode(value);
+            newHead->next = head;
+            head = newHead;
+        }
+        else {
+            SingleNode* prevHead = head;
+            head = new SingleNode(value);
+            head->next = prevHead;
+        }
+        size++;
+    }
+
+    // O(1)
+    void addToBack(uint32_t value)
+    {
+        if (head == nullptr && tail == nullptr)
+        {
+            head = tail = new SingleNode(value);
+        }
+        else if (head == tail)
+        {
+            SingleNode* newTail = new SingleNode(value);
+            tail->next = newTail;
+            tail = newTail;
+        }
+        else {
+            SingleNode* prevTail = tail;
+            tail = new SingleNode(value);
+            prevTail->next = tail;
+        }
+        size++;
+    }
+
+    // O(n)
+    void addAfterNode(uint32_t value, uint16_t idx)
+    {
+        if (size == 0)
+            return;
+        else if (idx > size)
+            return;
+        else if (idx == (size - 1))
+        {
+            SingleNode* newNode = new SingleNode(value);
+            tail->next = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            SingleNode* ptr = head;
+            for(int i = 0; i < idx; i++) {
+                ptr = ptr->next;
+            }
+            SingleNode* newNode = new SingleNode(value);
+
+            newNode->next = ptr->next;
+            ptr->next = newNode;
+        }
+        size++;
+    }
+
+    // O(n)
+    void addBeforeNode(uint32_t value, uint16_t idx)
+    {
+        if (size == 0)
+            return;
+        else if (idx < 0)
+            return;
+        else if (idx == 0)
+        {
+            SingleNode* newNode = new SingleNode(value);
+            newNode->next = head;
+            head = newNode;
+        }
+        else
+        {
+            SingleNode* ptr = head;
+            SingleNode* trailing = head;
+            for (int i = 0; i < idx; i++)
+            {
+                if (i == 1)
+                    trailing = head;
+                ptr = ptr->next;
+            }
+            SingleNode* newNode = new SingleNode(value);
+
+            newNode->next = ptr;
+            trailing->next = newNode;
+        }
+        size++;
+    }
+
+    // O(n)
+    void insert(uint32_t value) {
+        if (head == nullptr && tail == nullptr) {
+            addToFront(value);
+        }
+        else {
+            addToBack(value);
+        }
+    }
+
+    // O(n/2)
+    uint32_t getMiddle(void)
+    {
+        if (head == nullptr)
+            return -1;
+        if (head == tail)
+            return head->value;
+
+        SingleNode* slow = head;
+        SingleNode* fast = head;
+
+        if (head) {
+            while (fast != nullptr && fast->next != nullptr)
+            {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+        }
+        return slow->value;
+    }
+
+    // O(n)
+    void reverse(void)
+    {
+        if (head == nullptr)
+            return;
+        if (head == tail)
+            return;
+
+        SingleNode* prev = nullptr;
+        SingleNode* curr = nullptr;
+        SingleNode* ptr = head;
+
+        SingleNode* tempTail = tail;
+        tail = head;
+        head = tempTail;
+
+        while (ptr != nullptr)
+        {
+            curr = ptr;
+            ptr = ptr->next;
+
+            curr->next = prev;
+            prev = curr;
+        }
+    }
+
+    // O(n)
+    void printList(void)
+    {
+        SingleNode* ptr = head;
+        cout << "List of our values in the Single LL" << endl;
+        while (ptr != nullptr)
+        {
+            cout << ptr->value;
+            if (ptr != tail)
+            {
+                cout << "->";
+            }
+            ptr = ptr->next;
+        }
+        cout << endl;
+    }
+
+    // O(n)
+    void destroyList(void)
+    {
+        if(size == 0)
+            return;
+
+        SingleNode* ptr = head;
+        while(ptr != nullptr)
+        {
+            SingleNode* trailing = ptr;
+            ptr = ptr->next;
+
+            trailing->value = 0;
+            trailing->next = nullptr;
+            free(trailing);
+
+            size--;
+
+            cout << "This is size now: " << size << endl;
+        }
+    }
+};
